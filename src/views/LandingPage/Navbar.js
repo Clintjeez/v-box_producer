09bdee2f0c359440d64d7da6e5b2379ad4f1640c
ClintJeez <clintjeez@gmail.com>
2021-10-07 00:26:26 +0100

    initial commit with basic UI setup and styling (navbar)

---
+++ src/views/LandingPage/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Typography, Button } from "antd";
+
+import logoIcon from "../../assets/logo-icon.png";
+
+import "./styles/Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <nav className="nav-container">
+      <div className="container">
+        <div className="flex-container">
+          <div className="logo-container">
+            <Link to="/">
+              <img src={logoIcon} alt="logo" className="logo-img" />
+            </Link>
+            <Typography.Title level={2} className="logo-txt">
+              <Link to="/">V-BOX</Link>
+            </Typography.Title>
+          </div>
+          <Button className="nav-btn">
+            <Link to="/">SIGN IN</Link>
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
