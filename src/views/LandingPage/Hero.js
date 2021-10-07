8cc16d775f8e85a2aaf62e377f56d2bce6d6f149
ClintJeez <clintjeez@gmail.com>
2021-10-07 03:01:41 +0100

    hero section setup and styling

--- src/views/LandingPage/Hero.js
+++ src/views/LandingPage/Hero.js
@@ -1,7 +1,32 @@
 import React from "react";
+import { Typography, Button } from "antd";
+import { Link } from "react-router-dom";
 
+import "./styles/Hero.scss";
+
+const { Title, Text } = Typography;
 const Hero = () => {
-  return <section>Hero</section>;
+  return (
+    <section className="hero-container">
+      <div className="container">
+        <div className="hero-content">
+          <h3 className="hero-sub-title" level={3}>
+            A place for all movie producers
+          </h3>
+          <h1 className="hero-title" level={1}>
+            Upload your movies for FREE, get PAID for every VIEW on the platform
+          </h1>
+          <p className="hero-txt">
+            No marketers needed for your movie sales. Your viewers requires no
+            mobile data to stream on our platform
+          </p>
+          <Button className="hero-btn">
+            <Link to="/">UPLOAD MOVIES</Link>
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
