7029aeef7f37ecb2258ba3015f7187632a8bdba1
ClintJeez <clintjeez@gmail.com>
2021-10-11 16:11:53 +0100

    landing page ui components update

---
+++ src/services/moviesData.js
@@ -0,0 +1,48 @@
+import skinDeepImg from "../assets/movies/skin_deep-movie.png";
+import stalkerImg from "../assets/movies/stalker-movie.png";
+import proxyImg from "../assets/movies/proxy-movie.png";
+import loveIsWarImg from "../assets/movies/loveIsWar-movies.png";
+import fiveSixImg from "../assets/movies/five&six-movies.png";
+
+export const moviesData = [
+  {
+    id: 1,
+    title: "Skin Deep",
+    views: "1k",
+    likes: 3.6,
+    dislike: 1.5,
+    image: skinDeepImg,
+  },
+  {
+    id: 2,
+    title: "Stalker",
+    views: "1k",
+    likes: 3.6,
+    dislike: 1.5,
+    image: stalkerImg,
+  },
+  {
+    id: 3,
+    title: "Proxy",
+    views: "1k",
+    likes: 3.6,
+    dislike: 1.5,
+    image: proxyImg,
+  },
+  {
+    id: 4,
+    title: "Love is War",
+    views: "1k",
+    likes: 3.6,
+    dislike: 1.5,
+    image: loveIsWarImg,
+  },
+  {
+    id: 5,
+    title: "Five&Six",
+    views: "1k",
+    likes: 3.6,
+    dislike: 1.5,
+    image: fiveSixImg,
+  },
+];
--- src/views/LandingPage/index.js
+++ src/views/LandingPage/index.js
@@ -2,12 +2,18 @@ import React from "react";
 
 import Navbar from "./Navbar";
 import Hero from "./Hero";
+import Showcase from "./Showcase";
+import Producers from "./Producers";
+import MobileAppBanner from "./MobileAppBanner";
 
 const LandingPage = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Showcase />
+      <Producers />
+      <MobileAppBanner />
     </>
   );
 };
