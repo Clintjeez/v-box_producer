8f88cfa30e8a0d5d56cb3e1ac40522fe74e26338
ClintJeez <clintjeez@gmail.com>
2021-10-14 09:16:55 +0100

    movie showcase ui carousel update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import "./App.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import LandingPage from "./views/LandingPage";
 
